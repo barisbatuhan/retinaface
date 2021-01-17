997749b49cfd34477877cff31c097e5d022ccde6
barisbatuhan <barisbatuhantopal@gmail.com>
2021-01-17 16:44:30 +0300

    AP and mAP metrics are added

--- BBTNet/core/metrics.jl
+++ BBTNet/core/metrics.jl
@@ -8,38 +8,117 @@ Recall = TP / (TP + FN)
 
 """
 
-function evaluate_model(model; dataset="widerface", metric="mAP", iou_tholds=[0.5,0.05,0.95])
-    val_mat = matread("/datasets/widerface/wider_face_split/wider_face_val.mat")
-    facebox_list = val_mat["face_bbx_list"]
-    event_list = val_mat["event_list"]
-    file_list = val_mat["file_list"]
+function AP(gt, bboxes, confidences; iou_thold=0.5, thresh_num=1000, iou_vals=nothing, return_array=false)
+    results = zeros(2, thresh_num+1) # precisions 1st idx, recalls in 2nd idx
+    if iou_vals === nothing iou_vals = iou(gt[1:4,:], bboxes) end
     
+    selected_boxes = zeros(size(bboxes, 2)); selected_gts = zeros(size(gt, 2)); assigned_gts = 0;
     
+    pos_ious = findall(iou_vals .>= iou_thold)
+    pos_priors = getindex.(pos_ious, [1 2])[:,1]; pos_gts = getindex.(pos_ious, [1 2])[:,2];
+    sorted_priors = sortperm(confidences[2,pos_priors], rev=true)
+   
+    neg_ious = findall(iou_vals .< iou_thold); assigned_neg_gts = 0; 
+    neg_priors = getindex.(neg_ious, [1 2])[:,1]; sorted_neg_priors = sortperm(confidences[2,neg_priors], rev=true);
+    neg_gts = getindex.(neg_ious, [1 2])[:,2]; num_neg_gts = length(unique(neg_gts));
+    
+    for t in 1:thresh_num+1
+        thold = (t-1) / thresh_num
+        TP = 0; FP = 0; FN = 0;
+        for s in sorted_priors
+            prior = pos_priors[s]; gt_idx = pos_gts[s];  
+            if confidences[2, prior] >= thold && selected_boxes[prior] == 0 && selected_gts[gt_idx] == 0
+                # found TP example
+                TP += 1; assigned_gts += 1;  
+                selected_boxes[prior] = 1; selected_gts[gt_idx] = 1;
+            elseif confidences[2, prior] < thold
+                # unassigned positive gt boxes are counted as FN
+                FN = length(unique(pos_gts)) - assigned_gts 
+                break
+            end
+        end
+        
+        for ns in sorted_neg_priors
+            prior = neg_priors[ns]; gt_idx = neg_gts[ns];  
+            if confidences[2, prior] >= thold && selected_boxes[prior] == 0 && selected_gts[gt_idx] == 0
+                # found TP example
+                FP += 1; assigned_neg_gts += 1;  
+                selected_boxes[prior] = -1; selected_gts[gt_idx] = -1;
+            elseif confidences[2, prior] < thold || num_neg_gts == assigned_neg_gts
+                break
+            end
+        end
+        
+        precision = TP == 0 ? 0 : TP / (TP + FP)
+        recall = TP == 0 ? 0 : TP / (TP + FN)
+        results[1, t] = precision; results[2, t] = recall;
+    end
+
+    main_results = zeros(thresh_num+1)
+    for t in 1:thresh_num+1
+        thold = (t-1) / thresh_num
+        recall_indices = findall(results[2,:] .>= thold)
+        if length(recall_indices) == 0
+            main_results[t] = 0
+        else
+            main_results[t] = maximum(results[1,recall_indices])
+        end
+    end
+    
+    if return_array return main_results end
+    
+    AP = sum(main_results) / (thresh_num + 1)
+    return AP
 end
 
 
 
-function meanAP(gt, bboxes, confidences, iou_tholds; conf_thold=0.5)
-    conf_delta = 1000; num_iters = 0; num_faces = size(gt, 2); num_boxes = size(bboxes, 2);
-    curve = Dict() # keys are iou vals, values are [precision, recall]
+# function AP(gt, bboxes, confidences; iou_thold=0.5, thresh_num=1000, iou_vals=nothing, return_array=false)
+#     results = zeros(2, thresh_num+1) # precisions 1st idx, recalls in 2nd idx
+#     if iou_vals === nothing iou_vals = iou(gt[1:4,:], bboxes) end
     
-    iou_vals = iou(gt, bboxes)
-    # gets max values and indices for each gt
-    max_gt_vals, max_gt_idx = findmax(iou_vals; dims=1)
-    # gets max values and indices for each prior
-    max_prior_vals, max_prior_idx = findmax(iou_vals; dims=2) 
+#     max_gt_vals, max_gt_idx = findmax(iou_vals; dims=1) 
+#     pos_gt_indices =  getindex.(max_gt_idx[findall(max_gt_vals .>= iou_thold)], [1 2])[:,1]
+#     neg_gt_indices =  getindex.(max_gt_idx[findall(max_gt_vals .< iou_thold)], [1 2])[:,1]
     
-    for thold in iou_tholds[1]:iou_tholds[2]:iou_tholds[3]
-        TP = length(findall(max_gt_vals .>= thold))
-        FN = num_faces - TP
-        FP = 0
-        for t in 1:conf_delta
-            conf = 1 - t/conf_delta
-            
-        end
+#     for t in 1:thresh_num+1
+#         thold = (t-1) / thresh_num
+#         TP = size(findall(confidences[2,pos_gt_indices] .>= thold), 1)
+#         FP = size(findall(confidences[2,neg_gt_indices] .>= thold), 1)
+#         FN = size(findall(confidences[2,pos_gt_indices] .< thold), 1)
         
-        num_iters += 1
+#         precision = TP == 0 ? 0 : TP / (TP + FP)
+#         recall = TP == 0 ? 0 : TP / (TP + FN)
+#         results[1, t] = precision; results[2, t] = recall;
+#     end
+    
+#     main_results = zeros(thresh_num+1)
+#     for t in 1:thresh_num+1
+#         thold = (t-1) / thresh_num
+#         recall_indices = findall(results[2,:] .>= thold)
+#         if length(recall_indices) == 0
+#             main_results[t] = 0
+#         else
+#             main_results[t] = maximum(results[1,recall_indices])
+#         end
+#     end
+    
+#     if return_array return main_results end
+    
+#     AP = sum(main_results) / (thresh_num + 1)
+#     return AP
+# end
+
+
+
+function mAP(gt, bboxes, confidences, iou_tholds; thresh_num=1000)
+    APs = 0; cnt = 0; iou_vals = iou(gt, bboxes);
+    for iou_thold in iou_tholds[1]:iou_tholds[2]:iou_tholds[3]
+        APs += AP(gt, bboxes, confidences, iou_vals=iou_vals, thresh_num=1000, iou_thold=iou_thold)
+        cnt += 1
     end
+    mAP = APs / cnt
+    return mAP
 end
 
 """
