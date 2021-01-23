b2732b330649bc49d31417feb2d356a9f1de8c87
barisbatuhan <barisbatuhantopal@gmail.com>
2021-01-23 20:30:06 +0300

    Evaluation process update

--- evaluate.jl
+++ evaluate.jl
@@ -61,38 +61,82 @@ function main()
     )
     print("[INFO] Model is loaded!\n")
     
-#     hard = matread(eval_dir * "wider_hard_val.mat")
-#     med = matread(eval_dir * "wider_medium_val.mat")
-#     easy = matread(eval_dir * "wider_easy_val.mat")
-#     files = get_full_names(hard)
-    
     val_dir = wf_path * "val/"
     labels_dir = wf_labels_path * "val/"
-    d = WIDER_Data(val_dir, labels_dir, train=false, shuffle=true, batch_size=1, dtype=atype)
+    d = WIDER_Data(val_dir, labels_dir, train=false, shuffle=false, batch_size=1, dtype=atype)
     print("[INFO] Data is loaded!\n")
     
-    APs = 0; mAPs = 0; cnt = 0;
-    for file in d.files
-        img_paths = val_dir * "images/" * file
-        img, boxes = read_img(img_paths, boxes=d.bboxes[file]) 
-        reversed_img = reshape(reverse(img, dims=1), (size(img)..., 1))
-        imgs_arr = convert(atype, permutedims(reversed_img, (3,2,1,4)))
-        c, b, l = predict_image(model, imgs_arr, mode=mode, verbose=false)
-        AP_val = AP(boxes, b, c; iou_thold=0.5, thresh_num=1000)
-        mAP_val = mAP(boxes, b, c, mAP_ious, thresh_num=1000)
-        APs += AP_val; mAPs += mAP_val; cnt += 1;
-        
-        to_print = "Img: " * string(cnt) * " / " * string(length(d.files)) * " --> AP: " * string(AP_val) * " & mAP: " * string(mAP_val) * "\n"
-        print(to_print)
-        if log_file != nothing
-            open(log_file, "a") do io write(io, to_print) end;
+    hard = matread(eval_dir * "wider_hard_val.mat")
+    med = matread(eval_dir * "wider_medium_val.mat")
+    easy = matread(eval_dir * "wider_easy_val.mat")
+    
+    events = hard["event_list"]; esize = size(events, 1); files = hard["file_list"];
+    APs = zeros(3); mAP_hard = 0; cnts = zeros(3); total_cnt = 0;
+    for e in 1:esize
+        event = events[e, 1]
+        for f in 1:length(files[e])
+            file = files[e][f]
+            path = event * "/" * file * ".jpg"; img_path = val_dir * "images/" * path;
+            # box numbers are retrieved
+            hard_gt = Int64.(hard["gt_list"][e][f])
+            med_gt = Int64.(med["gt_list"][e][f])
+            easy_gt = Int64.(easy["gt_list"][e][f])
+            # reading the image
+            img, boxes = read_img(img_path, boxes=d.bboxes[path]) 
+            reversed_img = reshape(reverse(img, dims=1), (size(img)..., 1))
+            imgs_arr = convert(atype, permutedims(reversed_img, (3,2,1,4)))
+            # predicting the values
+            c, b, l = predict_image(model, imgs_arr, mode=mode, verbose=false)
+            # evaluate easy
+            if length(easy_gt) > 0
+                AP_val = 0
+                if typeof(easy_gt) <: Array
+                    AP_val = AP(boxes[:,easy_gt], b, c; iou_thold=0.5, thresh_num=1000)
+                else
+                    AP_val = AP(boxes[:,easy_gt:easy_gt], b, c; iou_thold=0.5, thresh_num=1000)
+                end
+                APs[1] += AP_val; cnts[1] += 1; 
+            end
+            # evaluate medium
+            if length(med_gt) > 0
+                AP_val = 0
+                if typeof(med_gt) <: Array
+                    AP_val = AP(boxes[:,med_gt], b, c; iou_thold=0.5, thresh_num=1000)
+                else
+                    AP_val = AP(boxes[:,med_gt:med_gt], b, c; iou_thold=0.5, thresh_num=1000)
+                end
+                APs[2] += AP_val; cnts[2] += 1; 
+            end
+            # evaluate hard
+            if length(hard_gt) > 0
+                AP_val = 0.0; mAP_val  = 0.0;
+                if typeof(hard_gt) <: Array
+                    AP_val += AP(boxes[:,hard_gt], b, c; iou_thold=0.5, thresh_num=1000)
+                    mAP_val += mAP(boxes[:,hard_gt], b, c, mAP_ious, thresh_num=1000)
+                else
+                    AP_val += AP(boxes[:,hard_gt:hard_gt], b, c; iou_thold=0.5, thresh_num=1000)
+                    mAP_val += mAP(boxes[:,hard_gt:hard_gt], b, c, mAP_ious, thresh_num=1000)
+                end
+                APs[3] += AP_val
+                mAP_hard += mAP_val
+                cnts[3] += 1
+            end
+            
+            total_cnt += 1
+            
+            AP_easy = round(APs[1] / max(1, cnts[1]), digits=4); 
+            AP_med = round(APs[2] / max(1, cnts[2]), digits=4);
+            AP_hard = round(APs[3] / max(1, cnts[3]), digits=4);
+            mAP_h = round(mAP_hard / max(1, cnts[3]), digits=4);
+            to_print = "Img: " * string(total_cnt) * " / " * string(length(d.files)) 
+            to_print *= " -->  APs = Easy: " * string(AP_easy) * " | Med: " * string(AP_med)
+            to_print *= " | Hard: " *string(AP_hard) * " && mAP Hard: " * string(mAP_h) * "\n"
+            print(to_print)
+            if log_file != nothing
+                open(log_file, "a") do io write(io, to_print) end;
+            end
         end
-    end  
-    to_print = "=== Total --> AP: " * string(APs / cnt) * " & mAP: " * string(mAPs / cnt) * "\n"
-    print(to_print)
-    if log_file != nothing
-        open(log_file, "a") do io write(io, to_print) end;
-    end
+    end 
 end
 
 main()
--- makefile
+++ makefile
@@ -13,4 +13,4 @@ predict:
 	julia predict.jl --mode 2 --laterals 3 --image_path "./data/images/test.jpg" --load_dir ./weights/PyTorch_R50_mode2_laterals3.jld2 --save_dir ./data/results/predicted.png
     
 evaluate:
-	julia evaluate.jl --mode 2 --laterals 3 --load_dir ./weights/PyTorch_R50_mode2_laterals3.jld2 --eval_dir ./data/ --log_file ./logs/evaluate_log.txt
+	julia evaluate.jl --mode 2 --laterals 3 --load_dir ./weights/PyTorch_R50_mode2_laterals3.jld2 --eval_dir ./data/ --log_file ./logs/evaluate_log_lat3_new.txt
